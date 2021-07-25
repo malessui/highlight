c4c0ea568d7a932f1aab0f6990892bfb3d6203b3
muhrusdi <mhmrofficial@gmail.com>
2021-07-25 16:14:17 +0800

    update story

--- packages/highlight/src/highlight.stories.tsx
+++ packages/highlight/src/highlight.stories.tsx
@@ -2,61 +2,72 @@ import React from 'react'
 import { Meta } from '@storybook/react';
 import ShiplessHigh from "./index"
 
-const codeSnippetBasic = `module.exports = {
-  ...
-  plugins: [
-    ...
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "cheatsheets",
-        path: "\${__dirname}/src/data/cheatsheets",src/data/blogs",
-      },
-    },
-    ...
-  ],
-}`
-const codeSnippetSingle = `module.exports = {
-  ...
-  plugins: [
-    ...
-    {
-      resolve: "gatsby-source-filesystem", // highlight-line
-      options: {
-        name: "cheatsheets",
-        path: "\${__dirname}/src/data/cheatsheets", // highlight-line
-      },
-    },
-    ...
-  ],
-}`
-
-const codeSnippetMulti = `module.exports = {
-  ...
-  plugins: [
-    ...
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        // highlight-remove-start
-        name: "cheatsheets",
-        path: "\${__dirname}/src/data/cheatsheets",
-        // highlight-remove-end
-        // highlight-add-start
-        name: "blogs",
-        path: "\${__dirname}/src/data/blogs",
-        // highlight-add-end
-      },
-    },
-    ...
-  ],
-}`
-
 export default {
   component: ShiplessHigh,
   title: 'Shipless',
+  paramaters: {
+    docs: {
+      source: {
+        type: "code"
+      }
+    }
+  }
 } as Meta;
 
-export const Basic: React.VFC<{}> = () => <ShiplessHigh code={codeSnippetBasic} lang="js"/>;
-export const InlineHighlight: React.VFC<{}> = () => <ShiplessHigh code={codeSnippetSingle} lang="js"/>;
-export const BlockHighlight: React.VFC<{}> = () => <ShiplessHigh code={codeSnippetMulti} lang="js"/>;
+export const Basic: React.VFC<{}> = () => {
+  const codeSnippetBasic = `module.exports = {
+    ...
+    plugins: [
+      ...
+      {
+        resolve: "gatsby-source-filesystem",
+        options: {
+          name: "cheatsheets",
+          path: "\${__dirname}/src/data/cheatsheets",src/data/blogs",
+        },
+      },
+      ...
+    ],
+  }`
+  return <ShiplessHigh code={codeSnippetBasic} lang="js"/>
+};
+export const InlineHighlight: React.VFC<{}> = () => {
+  const codeSnippetInline = `module.exports = {
+    ...
+    plugins: [
+      ...
+      {
+        resolve: "gatsby-source-filesystem", // highlight-line
+        options: {
+          name: "cheatsheets",
+          path: "\${__dirname}/src/data/cheatsheets", // highlight-line
+        },
+      },
+      ...
+    ],
+  }`
+  return <ShiplessHigh code={codeSnippetInline} lang="js"/>
+}
+export const BlockHighlight: React.VFC<{}> = () => {
+  const codeSnippetBlock = `module.exports = {
+    ...
+    plugins: [
+      ...
+      {
+        resolve: "gatsby-source-filesystem",
+        options: {
+          // highlight-remove-start
+          name: "cheatsheets",
+          path: "\${__dirname}/src/data/cheatsheets",
+          // highlight-remove-end
+          // highlight-add-start
+          name: "blogs",
+          path: "\${__dirname}/src/data/blogs",
+          // highlight-add-end
+        },
+      },
+      ...
+    ],
+  }`
+  return <ShiplessHigh code={codeSnippetBlock} lang="js"/>
+}
