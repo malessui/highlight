4305c3d7f20985f3d018ef85ae033c653889f689
muhrusdi <mhmrofficial@gmail.com>
2021-07-27 19:17:01 +0800

    fix last line deleted

--- packages/highlight/src/highlight.stories.tsx
+++ packages/highlight/src/highlight.stories.tsx
@@ -9,58 +9,58 @@ export default {
 
 export const Basic: React.VFC<{}> = () => {
   const codeSnippetBasic = `module.exports = {
+  ...
+  plugins: [
     ...
-    plugins: [
-      ...
-      {
-        resolve: "gatsby-source-filesystem",
-        options: {
-          name: "cheatsheets",
-          path: "\${__dirname}/src/data/cheatsheets",
-        },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "cheatsheets",
+        path: "\${__dirname}/src/data/cheatsheets",
       },
-      ...
-    ],
-  }`
+    },
+    ...
+  ],
+}`
   return <ShiplessHigh code={codeSnippetBasic} lang="js"/>
 };
 export const InlineHighlight: React.VFC<{}> = () => {
   const codeSnippetInline = `module.exports = {
+  ...
+  plugins: [
     ...
-    plugins: [
-      ...
-      {
-        resolve: "gatsby-source-filesystem", // highlight-line
-        options: {
-          name: "cheatsheets",
-          path: "\${__dirname}/src/data/cheatsheets", // highlight-line
-        },
+    {
+      resolve: "gatsby-source-filesystem", // highlight-line
+      options: {
+        name: "cheatsheets",
+        path: "\${__dirname}/src/data/cheatsheets", // highlight-line
       },
-      ...
-    ],
-  }`
+    },
+    ...
+  ],
+}`
   return <ShiplessHigh code={codeSnippetInline} lang="js"/>
 }
 export const BlockHighlight: React.VFC<{}> = () => {
   const codeSnippetBlock = `module.exports = {
+  ...
+  plugins: [
     ...
-    plugins: [
-      ...
-      {
-        resolve: "gatsby-source-filesystem",
-        options: {
-          // highlight-remove-start
-          name: "cheatsheets",
-          path: "\${__dirname}/src/data/cheatsheets",
-          // highlight-remove-end
-          // highlight-add-start
-          name: "blogs",
-          path: "\${__dirname}/src/data/blogs",
-          // highlight-add-end
-        },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        // highlight-remove-start
+        name: "cheatsheets",
+        path: "\${__dirname}/src/data/cheatsheets",
+        // highlight-remove-end
+        // highlight-add-start
+        name: "blogs",
+        path: "\${__dirname}/src/data/blogs",
+        // highlight-add-end
       },
-      ...
-    ],
-  }`
+    },
+    ...
+  ],
+}`
   return <ShiplessHigh code={codeSnippetBlock} lang="js"/>
 }
